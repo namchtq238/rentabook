961929b992dfacd445efd245ff3bd075c696d06d
nam_th <nam_th@flinters.vn>
2023-05-23 16:50:11 +0700

    add list and detail

--- src/main/java/com/rentabook/controller/UserController.java
+++ src/main/java/com/rentabook/controller/UserController.java
@@ -1,18 +1,31 @@
 package com.rentabook.controller;
 
+import com.rentabook.domain.Book;
 import com.rentabook.service.BookSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+    private static final int ITEMS_PER_PAGE = 5;
     @Autowired
     private BookSerivce bookSerivce;
 
-    @GetMapping("/user/book")
-    public String getListBook(Model model){
-        return "index";
+    @GetMapping("/books")
+    public String getBooks(Model model, @RequestParam(defaultValue = "1") int page) {
+        Page<Book> bookPage = bookSerivce.getByPage(page, ITEMS_PER_PAGE);
+        int totalPages = bookPage.getTotalPages();
+        model.addAttribute("books", bookPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        return "bookList";
+    }
+    @GetMapping("/books/detail")
+    public String getDetaliBook(Model model) {
+        return "productDetail";
     }
 }
--- src/main/java/com/rentabook/service/BookSerivce.java
+++ src/main/java/com/rentabook/service/BookSerivce.java
@@ -4,6 +4,8 @@ import com.rentabook.domain.Book;
 import com.rentabook.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,5 +27,9 @@ public class BookSerivce {
     }
     public void delete(Long id) {
         bookRepository.deleteById(id);
+    }
+
+    public Page<Book> getByPage(int page, int items){
+        return bookRepository.findAll(PageRequest.of(page - 1, items));
     }
 }
